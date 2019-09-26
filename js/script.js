ba07349c1d272e2a0d9520ce4a00fdb905d3f051
Paul <paul.ivanov1990@gmail.com>
2019-09-26 16:13:20 +0300

    HomeWork4

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 const score = document.querySelector('.score'),
       start = document.querySelector('.start'),
       gameArea = document.querySelector('.gameArea'),
-      car = document.createElement('div'),
-      music = document.createElement('embed');
+      car = document.createElement('div');
+      // music = document.createElement('embed'); для подключения музыки
 
-music.setAttribute('src', './audio.mp3');
-music.setAttribute('type', 'audio/mp3');
-music.classList.add('music');
+      
+const audio = new Audio('./audio.mp3'),
+      crash = new Audio('./crash.mp3');
+let allow = false;
+audio.volume = 0.1;
+crash.volume = 0.2;
+audio.addEventListener('loadeddata', () => {
+  allow = true;
+});
+
+// так можно подлкючить музыку другим способом с упправлением
+// music.setAttribute('src', './audio.mp3');
+// music.setAttribute('type', 'audio/mp3');
+// music.classList.add('music');
 
 car.classList.add('car');
+
+
 
 start.addEventListener('click', startGame);
 
@@ -25,18 +38,36 @@ const keys = {
 const setting = {
   start: false,
   score: 0,
-  speed: 3,
-  traffic: 3
+  speed: 5,
+  traffic: 3,
+  level: 0
 };
 
 function gerQuantityElements(heightElement) {
-  return Math.ceil(gameArea.offsetHeight / heightElement);  
+  return Math.ceil(gameArea.offsetHeight / heightElement) + 1;  
 }
 
-function startGame() {  
+function startGame(event) {
+
+  if(event.target.classList.contains('start')) {
+    return;
+  } 
+  if (event.target.classList.contains('easy')) {
+    setting.speed = 3;
+    setting.traffic = 3;
+  }
+  if (event.target.classList.contains('medium')) {
+    setting.speed = 5;
+    setting.traffic = 3;
+  }
+  if (event.target.classList.contains('hard')) {
+    setting.speed = 7;
+    setting.traffic = 2;
+  }
+
   start.classList.add('hide');
   gameArea.innerHTML = '';  
-  for (let i = 0; i < gerQuantityElements(100) + 1; i++) {
+  for (let i = 0; i < gerQuantityElements(100); i++) {
     const line = document.createElement('div');
     line.classList.add('line');
     line.style.top = (i * 100) + 'px';
@@ -46,7 +77,7 @@ function startGame() {
 
   for (let i = 0; i < gerQuantityElements(100 * setting.traffic); i++) {
     const enemy = document.createElement('div');
-    let enemyImg = Math.floor(Math.random() * 2) + 1;
+    let enemyImg = Math.floor(Math.random() * 4) + 1;
     enemy.classList.add('enemy');
     enemy.y = -100 * setting.traffic * (i + 1);
     enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
@@ -55,42 +86,60 @@ function startGame() {
     gameArea.appendChild(enemy);
   }
   
+  if (allow) {
+    audio.play();
+  }
+  
   setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
   car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
   car.style.top = 'auto';
   car.style.bottom = '10px';
-  gameArea.appendChild(music);
+  // gameArea.appendChild(music); так тоже можно запустить музыку
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
 }
 
 function playGame() {
-  if (setting.start) {
-    setting.score += setting.speed;
-    score.innerHTML = `Score:<br>${setting.score}`;
-    moveRoad();
-    moveEnemy();
-    if (keys.ArrowLeft && setting.x > 0) {
-      setting.x -= setting.speed;
-    }
-    if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
-      setting.x += setting.speed;
-    }
-    if (keys.ArrowUp && setting.y > 0) {
-      setting.y -= setting.speed;
-    }
-    if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
-      setting.y += setting.speed;
-    }
-    car.style.left = setting.x + 'px';
-    car.style.top = setting.y + 'px';
-    requestAnimationFrame(playGame);
-  } else {
-    music.remove();
+  
+  if (setting.score > 2000 && setting.level === 0) {
+    ++setting.speed;
+    ++setting.level;
+  } else if(setting.score > 5000 && setting.level === 1) {
+    ++setting.speed;
+    ++setting.level;
+  } else if (setting.score > 10000 && setting.level === 2){
+    ++setting.speed;
+    ++setting.level;
   }
+
+  setting.score += setting.speed;
+  score.innerHTML = `Score<br>${setting.score}`;
+  moveRoad();
+  moveEnemy();
+  if (keys.ArrowLeft && setting.x > 0) {
+    setting.x -= setting.speed;
+  }
+  if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+    setting.x += setting.speed;
+  }
+  if (keys.ArrowUp && setting.y > 0) {
+    setting.y -= setting.speed;
+  }
+  if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+    setting.y += setting.speed;
+  }
+  car.style.left = setting.x + 'px';
+  car.style.top = setting.y + 'px';
+
+  if (setting.start) {
+    requestAnimationFrame(playGame);
+  }
+  // else {
+  //   music.remove(); для отмены музыки, второй способ
+  // }
 }
 
 function startRun(event) {
@@ -120,22 +169,45 @@ function moveRoad() {
 
 function moveEnemy() {
   let enemy = document.querySelectorAll('.enemy');
-  enemy.forEach(function(item){
+  enemy.forEach(function(item) {
     let carRect = car.getBoundingClientRect();
     let enemyRect = item.getBoundingClientRect();
-    if(carRect.top <= enemyRect.bottom &&
-      carRect.right >= enemyRect.left &&
-      carRect.left <= enemyRect.right &&
-      carRect.bottom >= enemyRect.top) {
+    if(carRect.top + 5 <= enemyRect.bottom &&
+      carRect.right + 5 >= enemyRect.left &&
+      carRect.left + 5 <= enemyRect.right &&
+      carRect.bottom + 5 >= enemyRect.top) {
         setting.start = false;
-        start.classList.remove('hide');
-        start.style.top = score.offsetHeight;
+        audio.pause();
+        crash.play();
+
+      let topScore = localStorage.getItem('topScore');
+      if (topScore < setting.score) {
+        localStorage.setItem('topScore', setting.score);
+        score.innerHTML = `
+        Score<br>
+        ${setting.score}<br>
+        Congratulations!<br>
+        You set a new best score: ${setting.score}
+        `;
+      } else {
+        score.innerHTML = `
+        Score<br>
+        ${setting.score}<br>
+        You best score: ${topScore}
+        `;
+      }
+      
+      // console.warn('ДТП');
+      start.classList.remove('hide');
+      start.style.top = score.offsetHeight;
     }
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
     if(item.y >= gameArea.offsetHeight) {
+      let enemyImg = Math.floor(Math.random() * 4) + 1;
       item.y = -100 * setting.traffic;
       item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+      item.style.background = `transparent url('../img/enemy${enemyImg}.png') center / cover no-repeat`;
     }
   });
 }
